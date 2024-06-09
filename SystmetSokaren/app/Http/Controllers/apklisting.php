8858d247f34f4424e7bbe40c7d7c723883e99b29
Lucas-kantola <48250445+LucasKantola@users.noreply.github.com>
2024-06-09 18:53:06 +0200

    slow as shit search and data handler

--- SystmetSokaren/app/Http/Controllers/apklisting.php
+++ SystmetSokaren/app/Http/Controllers/apklisting.php
@@ -29,13 +29,14 @@ class apklisting extends Controller
         $stores = $stores['siteSearchResults'];
 
         foreach($stores as $item){
-            $data = (object)[
-                "siteId" => $item['siteId'],
-                "displayName" => $item['displayName'],
-                "city" => $item['city'],
-            ];
-
-            array_push($output, $data);
+            if (strpos($item["siteId"], '-') == false) {
+                $data = (object)[
+                    "siteId" => $item['siteId'],
+                    "displayName" => $item['displayName'],
+                    "city" => $item['city'],
+                ];
+                array_push($output, $data);
+            }
         } 
 
         return $output;
@@ -43,20 +44,42 @@ class apklisting extends Controller
     function index(){
         $stores = $this->getStores();
 
+        $products = [];
+
         $url = "https://api-extern.systembolaget.se/sb-api-ecommerce/v1/productsearch/search?size=30";
 
         $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', $url, [
-            'headers' => [
-                "accept" => "application/json",
-                "access-control-allow-origin" => "*",
-                "ocp-apim-subscription-key" => "cfc702aed3094c86b92d6d4ff7a54c84",
-                "Referer" => "https://www.systembolaget.se/",
-            ],
-            'verify' => false, // Disable SSL verification
-        ])
-        ->getBody()
-        ->getContents();
-        return view(index, ['stores' => $stores, 'products' => json_decode($response, true)]);
+
+        for($i = 1; $i < 100; $i++){
+            $response = $client->request('GET', $url . "&page=" . $i, [
+                'headers' => [
+                    "accept" => "application/json",
+                    "access-control-allow-origin" => "*",
+                    "ocp-apim-subscription-key" => "cfc702aed3094c86b92d6d4ff7a54c84",
+                    "Referer" => "https://www.systembolaget.se/",
+                ],
+                'verify' => false, // Disable SSL verification
+            ])->getBody()->getContents();
+            $data = json_decode($response, true);
+            
+            if(!($data["metadata"]["nextPage"] > $i)){
+                break;
+            }
+            $data = $data["products"];
+
+            foreach($data as $item){
+                $send = (object)[
+                    "name" => $item["productNameBold"] . ";" . $item["productNameThin"],
+                    "price" => $item["price"],
+                    "volume" => $item["volume"],
+                    "alcoholPercentage" => $item["alcoholPercentage"],
+                    "url" => "systembolaget.se/" . $item["productNumber"],
+                ];
+                array_push($products, $send);
+            }
+        }
+
+        return $products;
+        // return view("index", ['stores' => $stores, 'products' => $products]);
     }
 }
